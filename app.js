e47137aabbeea2c7462c78dee5eb32ee70d2aaf6
Pavel Kutakov <pavel.kutakov@huawei.com>
2021-06-10 12:11:35 +0300

    one more time

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
-const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3003;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World - version 10');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+exports.handler = async (event, context) => {
+    const output =
+    {
+        'statusCode': 200,
+        'headers':
+        {
+            'Content-Type': 'application/json'
+        },
+        'isBase64Encoded': false,
+        'body': 'NEW FUNCTION CREATED VIA API',
+    }
+    return output;
+}
