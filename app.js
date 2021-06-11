1a3d643bbdf52fa57cd07f3748d283cec9bc869d
Pavel Kutakov <pavel.kutakov@huawei.com>
2021-06-11 10:59:27 +0300

    one more time

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ exports.handler = async (event, context) => {
             'Content-Type': 'application/json'
         },
         'isBase64Encoded': false,
-        'body': 'NEW FUNCTION CREATED VIA API (1)'
+        'body': 'NEW FUNCTION CREATED VIA API (2)'
     }
     return output;
 }
